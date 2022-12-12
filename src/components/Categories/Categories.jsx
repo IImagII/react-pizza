a9d783569625cf3dc3864ed60fc2dcfb720f79a0
iimagii <mag_1@ukr.net>
2022-12-12 15:45:29 +0200

    сделал чтобы передавать индекс кнопке

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -2,17 +2,50 @@ import React, { useState } from 'react'
 
 export const Categories = () => {
    const [activeIndex, setActiveIndex] = useState(3)
+
+   const onClickCategory = index => {
+      setActiveIndex(index)
+   }
+
    return (
       <div className='categories'>
          <ul>
-            <li className={activeIndex === 0 ? 'active' : ''}>Все</li>
-            <li className={activeIndex === 1 ? 'active' : ''}>Мясные</li>
-            <li className={activeIndex === 2 ? 'active' : ''}>
+            <li
+               className={activeIndex === 0 ? 'active' : ''}
+               onClick={() => onClickCategory(0)}
+            >
+               Все
+            </li>
+            <li
+               className={activeIndex === 1 ? 'active' : ''}
+               onClick={() => onClickCategory(1)}
+            >
+               Мясные
+            </li>
+            <li
+               className={activeIndex === 2 ? 'active' : ''}
+               onClick={() => onClickCategory(2)}
+            >
                Вегетарианская
             </li>
-            <li className={activeIndex === 3 ? 'active' : ''}>Гриль</li>
-            <li className={activeIndex === 4 ? 'active' : ''}>Острые</li>
-            <li className={activeIndex === 5 ? 'active' : ''}>Закрытые</li>
+            <li
+               className={activeIndex === 3 ? 'active' : ''}
+               onClick={() => onClickCategory(3)}
+            >
+               Гриль
+            </li>
+            <li
+               className={activeIndex === 4 ? 'active' : ''}
+               onClick={() => onClickCategory(4)}
+            >
+               Острые
+            </li>
+            <li
+               className={activeIndex === 5 ? 'active' : ''}
+               onClick={() => onClickCategory(5)}
+            >
+               Закрытые
+            </li>
          </ul>
       </div>
    )
