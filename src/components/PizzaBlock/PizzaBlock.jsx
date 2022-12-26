a914a0ddfa63d62c08ebdd622950c3ee8c1b501a
iimagii <mag_1@ukr.net>
2022-12-26 21:12:48 +0200

    переделал ошибки при слиянии

--- src/components/PizzaBlock/PizzaBlock.jsx
+++ src/components/PizzaBlock/PizzaBlock.jsx
@@ -12,12 +12,8 @@ export const PizzaBlock = ({ id, title, price, imageUrl, types, sizes }) => {
    const typeNames = ['тонкое ', 'традиционное ']
    const dispatch = useDispatch()
 
-<<<<<<< HEAD
    const addCount = cartItem ? cartItem.count : 0 // делаем проверку есть ли товар в корзине и взависимости от этого выддаем число товара в корзинеы
 
-=======
-   const addCount = cartItem ? cartItem.count : 0
->>>>>>> 74473a1f3742c35adcd93afd8cca63e39111fc3d
    const handleAddItems = () => {
       const item = {
          id,
@@ -80,13 +76,9 @@ export const PizzaBlock = ({ id, title, price, imageUrl, types, sizes }) => {
                   />
                </svg>
                <span>Добавить</span>
-<<<<<<< HEAD
                {addCount > 0 && <i>{addCount}</i>}
                {/* делаем чтобы показывалось
                сколько товарра заказывалось в штуках */}
-=======
-               <i>{addCount}</i>
->>>>>>> 74473a1f3742c35adcd93afd8cca63e39111fc3d
             </button>
          </div>
       </div>
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,16 +1,5 @@
 import React from 'react'
-import { NavLink, useNavigate } from 'react-router-dom'
-<<<<<<< HEAD
-import cartEmpty from './../../assets/img/empty-cart.png'
-import { useDispatch, useSelector } from 'react-redux'
-import { OneCart } from './OneCart'
-import { clearItems } from '../../store/Slice/cartSlice'
-
-export const Cart = () => {
-   const { totalPrice, items } = useSelector(state => state.carts)
-
-   const dispatch = useDispatch()
-=======
+import { NavLink } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { ItemCart } from './ItemCart'
 import { clearItems, selectorCarts } from '../../store/Slice/cartSlice'
@@ -19,7 +8,7 @@ import { CartEmpty } from './CartEmpty'
 export const Cart = () => {
    const { totalPrice, items, addCount } = useSelector(selectorCarts)
    const dispatch = useDispatch()
-   const navigate = useNavigate()
+
    const handleClearItems = () => {
       dispatch(clearItems())
    }
@@ -27,7 +16,7 @@ export const Cart = () => {
    if (!totalPrice) {
       return <CartEmpty />
    }
->>>>>>> 74473a1f3742c35adcd93afd8cca63e39111fc3d
+
    return (
       <div className='content'>
          <div className='container container--cart'>
@@ -103,22 +92,12 @@ export const Cart = () => {
                         />
                      </svg>
 
-<<<<<<< HEAD
-                     <span onClick={() => dispatch(clearItems())}>
-                        Очистить корзину
-                     </span>
-=======
                      <span onClick={handleClearItems}>Очистить корзину</span>
->>>>>>> 74473a1f3742c35adcd93afd8cca63e39111fc3d
                   </div>
                </div>
                <div className='cart__items'>
                   {items.map(item => (
-<<<<<<< HEAD
-                     <OneCart {...item} key={item.id} />
-=======
                      <ItemCart key={item.id} {...item} />
->>>>>>> 74473a1f3742c35adcd93afd8cca63e39111fc3d
                   ))}
                </div>
                <div className='cart__bottom'>
@@ -129,11 +108,7 @@ export const Cart = () => {
                      </span>
                      <span>
                         {' '}
-<<<<<<< HEAD
                         Сумма заказа: <b>{totalPrice} ₴</b>{' '}
-=======
-                        Сумма заказа: <b>{totalPrice} ₴</b>
->>>>>>> 74473a1f3742c35adcd93afd8cca63e39111fc3d
                      </span>
                   </div>
                   <div className='cart__bottom-buttons'>
